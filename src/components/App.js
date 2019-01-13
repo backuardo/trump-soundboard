5603a9aa039d83e38f7579ac5db94e63ef09a776
cameoapp <benjamin.eisner.20@dartmouth.edu>
2019-01-13 17:52:55 -0500

    scrolling

--- src/components/App.js
+++ src/components/App.js
@@ -1,6 +1,7 @@
 import React, { Component } from "react";
 import Head from "./Head";
 import SoundButton from "./SoundButton";
+import githubLogo from "../images/github-logo.png";
 import "./App.css";
 
 //import china from "../data/sounds/i_like_china.mp3";
@@ -26,21 +27,25 @@ class App extends Component {
   render() {
     return (
       <div className="app">
-        <h1>Trump Soundboard</h1>
         <Head isSpeaking={this.state.isSpeaking} />
         <div className="sounds">
-          {sounds.map(sound => {
-            return (
-              <SoundButton
-                sound={sound.mp3}
-                desc={sound.desc}
-                startAnimation={this.startHeadAnimation}
-                endAnimation={this.endHeadAnimation}
-                key={sound.desc}
-              />
-            );
-          })}
+          <div className="scrollbar">
+            {sounds.map(sound => {
+              return (
+                <SoundButton
+                  sound={sound.mp3}
+                  desc={sound.desc}
+                  startAnimation={this.startHeadAnimation}
+                  endAnimation={this.endHeadAnimation}
+                  key={sound.desc}
+                />
+              );
+            })}
+          </div>
         </div>
+        <a href="https://github.com/beneisnr/trump-soundboard">
+          <img src={githubLogo} width="50px" alt="github logo" />
+        </a>
       </div>
     );
   }
